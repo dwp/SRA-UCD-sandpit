7507719cc5c3259c28c42e5891a64296f3169fd5
Colin <colin.oakley@engineering.digital.dwp.gov.uk>
2024-05-30 11:37:47 +0100

    feat: add routes for home ownership

--- app/routes.js
+++ app/routes.js
@@ -8,7 +8,7 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-
+// relationship urls
 router.post("/mvp/account/relationship-changes/have-you-moved-in-with-a-partner", (req, res) => {
     if (req.body.movedInWithPartner === "yes") {
         res.redirect("/mvp/account/relationship-changes/partner-details")
@@ -30,7 +30,27 @@ router.post("/mvp/account/relationship-changes/does-anyone-live-in-the-property"
     }
 })
 
-
 router.post("/mvp/account/relationship-changes/check-your-answers", (_req, res) => {
+    res.redirect("/mvp/account/to-do")
+})
+
+// home ownership urls
+router.post("/mvp/account/home-ownership/is-the-address-you-have-moved-to-rented-or-owned", (_req, res) => {
+    res.redirect("/mvp/account/home-ownership/did-you-own-your-own-home")
+})
+
+router.post("/mvp/account/home-ownership/did-you-own-your-own-home", (_req, res) => {
+    res.redirect("/mvp/account/home-ownership/do-you-have-a-mortgage-on-your-new-address")
+})
+
+router.post("/mvp/account/home-ownership/do-you-have-a-mortgage-on-your-new-address", (_req, res) => {
+    res.redirect("/mvp/account/home-ownership/do-you-pay-service-charges")
+})
+
+router.post("/mvp/account/home-ownership/do-you-pay-service-charges", (_req, res) => {
+    res.redirect("/mvp/account/home-ownership/check-your-answers")
+})
+
+router.post("/mvp/account/home-ownership/check-your-answers", (_req, res) => {
     res.redirect("/mvp/account/to-do")
 })
