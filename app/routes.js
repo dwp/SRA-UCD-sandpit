81b1f03b67da3068c15b5ae6bd39215baaf4ad27
Colin <colin.oakley@engineering.digital.dwp.gov.uk>
2024-05-30 10:52:57 +0100

    feat: adding example mvp route

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,30 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+router.post("/mvp/account/relationship-changes/have-you-moved-in-with-a-partner", (req, res) => {
+    if (req.body.movedInWithPartner === "yes") {
+        res.redirect("/mvp/account/relationship-changes/partner-details")
+    } else {
+        res.redirect("/mvp/account/relationship-changes/does-anyone-live-in-the-property")
+    }
+})
+
+router.post("/mvp/account/relationship-changes/partner-details", (req, res) => {
+    res.redirect("/mvp/account/relationship-changes/does-anyone-live-in-the-property")
+})
+
+
+router.post("/mvp/account/relationship-changes/does-anyone-live-in-the-property", (req, res) => {
+    if (req.body.anyLiveInTheProperty === "yes") {
+        res.redirect("/mvp/account/relationship-changes/people-who-live-at-your-addres")
+    } else {
+        res.redirect("/mvp/account/relationship-changes/check-your-answers")
+    }
+})
+
+
+router.post("/mvp/account/relationship-changes/check-your-answers", (_req, res) => {
+    res.redirect("/mvp/account/to-do")
+})
